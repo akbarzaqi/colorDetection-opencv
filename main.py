98e101ab129b0fd11cad5d51c541b6cf2b414190
akbarzaqi <zakiakbar143@gmail.com>
2024-12-30 07:33:07 +0700

    add goal and ball detection

--- main.py
+++ main.py
@@ -30,11 +30,11 @@ jpg_img = read_image(root_path)
 resize_img = cv2.resize(jpg_img, (320, 240))
 
 hsv_img = cv2.cvtColor(resize_img, cv2.COLOR_RGB2HSV)
-
+kernel = np.ones((15, 15), np.uint8)
 
 while True:
    
-    
+    #buat nyari nilai warna hsv
     h_minGawang = cv2.getTrackbarPos("H_MIN", "Gawang")
     h_maxGawang = cv2.getTrackbarPos("H_MAX", "Gawang")
     s_minGawang = cv2.getTrackbarPos("S_MIN", "Gawang")
@@ -42,26 +42,47 @@ while True:
     v_minGawang = cv2.getTrackbarPos("V_MIN", "Gawang")
     v_maxGawang = cv2.getTrackbarPos("V_MAX", "Gawang")
     
-    lower_yellow = np.array([h_minGawang, s_minGawang, v_minGawang])
-    upper_yellow = np.array([h_maxGawang, s_maxGawang, v_maxGawang])
+    
+    
+    lower_yellow = np.array([19, 106, 91])
+    upper_yellow = np.array([31, 255, 255])
+    
+    lower_red = np.array([132, 219, 179])
+    upper_red = np.array([179, 255, 255])
+    
+    maskBola = cv2.inRange(hsv_img, lower_red, upper_red)
+    resultBola = cv2.bitwise_and(resize_img, resize_img, mask = maskBola)
+    bolaDilasi = cv2.dilate(maskBola, kernel, iterations=1) 
     
     maskGawang = cv2.inRange(hsv_img, lower_yellow, upper_yellow)
     resultGawang = cv2.bitwise_and(resize_img, resize_img, mask = maskGawang)
+    cvtGawang = cv2.cvtColor(resultGawang, cv2.COLOR_RGB2HSV)
     
     copy_img = resize_img.copy()
     
     contours, _ = cv2.findContours(maskGawang, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    detekBola, _ = cv2.findContours(bolaDilasi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
     if len(contours) != 0:
         for contour in contours:
-            if cv2.contourArea(contour) > 100:
+            if cv2.contourArea(contour) > 1000:
                 x, y, w, h = cv2.boundingRect(contour)
                 cv2.rectangle(copy_img, (x,y), (x + w, y + h), (0, 255, 0), 2)
-                text_position = (x, y - 10)  
-                cv2.putText(copy_img, "Gawang", text_position, cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 1, cv2.LINE_AA)
+                posisiTeksGawang = (x, y - 10)  
+                cv2.putText(copy_img, "Gawang", posisiTeksGawang, cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 1, cv2.LINE_AA)
+
+    if len(detekBola) != 0:
+        for bola in detekBola:
+            if cv2.contourArea(bola) > 1:
+                x, y, w, h = cv2.boundingRect(bola)
+                cv2.rectangle(copy_img, (x,y), (x + w, y + h), (0, 0, 255), 2)
+                posisiTeksBola = (x, y - 10)  
+                cv2.putText(copy_img, "Bola", posisiTeksBola, cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 1, cv2.LINE_AA)                    
     
     cv2.imshow("mask gawang", maskGawang)
-    cv2.imshow("result gawang", resultGawang)
+    cv2.imshow("result gawang", cvtGawang)
+    cv2.imshow("mask bola", maskBola)
+    cv2.imshow("result bola", resultBola)
     show_image(copy_img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
